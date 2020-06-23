2899cf22d6e4c82aa7614299498a55a7bd64a581
Aj0SK <andrejkorman@gmail.com>
2020-06-23 21:10:28 +0200

    Done requesting bodies

--- src/main.rs
+++ src/main.rs
@@ -12,12 +12,17 @@ use http::{HeaderValue, header::{COOKIE}};
 use futures::{stream, StreamExt};
 
 use std::collections::HashMap;
+use std::fs;
+use std::fs::File;
+use std::io::prelude::*;
+
+const MAX_COURSES: i32 = 10;
 
 fn main() {
     let cookie1 = format!("anketasessid_FMFI_prod={}", SECRET1);
     let cookie2 = format!("cosign-proxy-anketa.uniba.sk={}", SECRET2);
     let url = "https://anketa.uniba.sk/fmph/vysledky";
-    let base = "https://anketa.uniba.sk";
+    //let base = "https://anketa.uniba.sk";
 
     //get_course(&url, &cookie1, &cookie2);
 
@@ -25,7 +30,7 @@ fn main() {
     let main_page_body: Vec<String> = helper(main_page, &cookie1, &cookie2);
     let year_links: Vec<String> = parse_main_page(&main_page_body[0]);
 
-    let year_links_abs: Vec<String> = year_links.clone().iter().map(|x| format!("{}{}/predmety/", base, x)).collect();
+    let year_links_abs: Vec<String> = year_links.clone().iter().map(|x| format!("{}/{}/predmety/", url, x)).collect();
     let year_bodies: Vec<String> = helper(year_links_abs.clone(), &cookie1, &cookie2);
 
     let mut mymap : HashMap<String, Vec<String>> = HashMap::new();
@@ -37,14 +42,44 @@ fn main() {
         }
     }
 
-    for (key, value) in mymap.into_iter() {
+    fs::create_dir_all("./data").unwrap();
+
+    let mut parsed_urls: Vec<String> = Vec::new();
+    let mut parsed_years: Vec<String> = Vec::new();
+    let mut parsed_courses: Vec<String> = Vec::new();
+
+    let mut counter: i32 = 0;
+    'outer: for (key, value) in mymap.into_iter() {
         print!("{} ->", key);
         for v in value {
             print!(" {}", v);
+
+            let curr_url = format!("{}/{}/predmet/{}", url, v, key);
+
+            parsed_years.push(v.clone());
+            parsed_courses.push(key.clone());
+            parsed_urls.push(curr_url.clone());
+
+            counter += 1;
+            if counter == MAX_COURSES {
+                break 'outer;
+            }
         }
         println!();
     }
 
+    let requested_course_bodies: Vec<String> = helper(parsed_urls, &cookie1, &cookie2);
+
+    for i in 0..requested_course_bodies.len() {
+        let path = format!("./data/{}&{}", parsed_years[i], parsed_courses[i]);
+        let mut file = match File::create(&path) {
+            Err(why) => panic!("couldn't create {}: {}", path, why),
+            Ok(file) => file,
+        };
+        file.write_all(&requested_course_bodies[i].as_bytes()).unwrap();
+    }
+
+    println!("\nCounter is {}", counter);
 }
 
 fn parse_year(body: &str) -> Vec<String> {
@@ -73,7 +108,8 @@ fn parse_main_page(body: &str) -> Vec<String> {
 
     for story in fragment.select(&stories1) {
         let x = story.value().attr("href").unwrap();
-        results.push(x.to_string());
+        let y = x.to_string().rfind("/vysledky/").unwrap_or(0);
+        results.push(x.to_string()[y+10..].to_string());
     }
 
     return results;
@@ -121,7 +157,7 @@ async fn helper(urls: Vec<String>, cookie1: &str, cookie2: &str) -> std::vec::Ve
     return results;
 }
 
-fn parse_course(body: &str) {
+/*fn parse_course(body: &str) {
 
     let fragment = Html::parse_document(&body);
     let stories1 = Selector::parse(r#"h2[class="fragment"]"#).unwrap();
@@ -143,7 +179,7 @@ fn parse_course(body: &str) {
         println!("{:?}", story_txt[0].trim());
     }
 
-}
+}*/
 
 /*fn get_course(url: &str, cookie1: &str, cookie2: &str) {
     use reqwest::blocking::Client;

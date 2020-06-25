1c55a83e76d91e72ec9d1d7408cad9f0b915544c
Aj0SK <andrejkorman@gmail.com>
2020-06-25 12:53:16 +0200

    Divide requesting and parsing

--- src/main.rs
+++ src/main.rs
@@ -10,14 +10,68 @@ use crate::parse_html::parse_year;
 use crate::secret::SECRET1;
 use crate::secret::SECRET2;
 
-use std::collections::HashMap;
+//use std::collections::HashMap;
 use std::fs;
 use std::fs::File;
 use std::io::prelude::*;
 
+use std::path::Path;
+
 const MAX_COURSES: i32 = 10;
 
 fn main() {
+    if Path::new("./data").exists() {
+        println!("Reuse old data.");
+    } else {
+        println!("Create new data.");
+        prepare_data();
+    }
+
+    let data_paths = fs::read_dir("./data").unwrap();
+
+    let mut parsed_bodies: Vec<String> = Vec::new();
+    let mut parsed_years: Vec<String> = Vec::new();
+    let mut parsed_courses: Vec<String> = Vec::new();
+
+    for path in data_paths {
+        let path_str = path.unwrap().path().into_os_string().into_string().unwrap();
+        let vec: Vec<&str> = path_str.split("&").collect();
+
+        assert_eq!(vec.len(), 2);
+
+        let year = vec[0];
+        let course = vec[1];
+
+        parsed_years.push(year.to_string());
+        parsed_courses.push(course.to_string());
+
+        let mut contents = String::new();
+        File::open(path_str)
+            .unwrap()
+            .read_to_string(&mut contents)
+            .unwrap();
+        parsed_bodies.push(contents.clone());
+    }
+
+    println!("Len of parsed is {}", parsed_bodies.len());
+
+    /*
+    let mut mymap: HashMap<String, Vec<String>> = HashMap::new();
+    for (key, value) in mymap.into_iter() {
+        print!("{} ->", key);
+        for v in value {
+            print!(" {}", v);
+        }
+        println!();
+    }
+    for i in 0..year_links_abs.len() {
+        for j in parse_year(&year_bodies[i]) {
+            mymap.entry(j).or_default().push(year_links[i].clone());
+        }
+    }*/
+}
+
+fn prepare_data() {
     let cookie1 = format!("anketasessid_FMFI_prod={}", SECRET1);
     let cookie2 = format!("cosign-proxy-anketa.uniba.sk={}", SECRET2);
     let url = "https://anketa.uniba.sk/fmph/vysledky";
@@ -36,14 +90,6 @@ fn main() {
         .collect();
     let year_bodies: Vec<String> = helper(year_links_abs.clone(), &cookie1, &cookie2);
 
-    let mut mymap: HashMap<String, Vec<String>> = HashMap::new();
-
-    for i in 0..year_links_abs.len() {
-        for j in parse_year(&year_bodies[i]) {
-            mymap.entry(j).or_default().push(year_links[i].clone());
-        }
-    }
-
     fs::create_dir_all("./data").unwrap();
 
     let mut parsed_urls: Vec<String> = Vec::new();
@@ -51,15 +97,13 @@ fn main() {
     let mut parsed_courses: Vec<String> = Vec::new();
 
     let mut counter: i32 = 0;
-    'outer: for (key, value) in mymap.into_iter() {
-        print!("{} ->", key);
-        for v in value {
-            print!(" {}", v);
 
-            let curr_url = format!("{}/{}/predmet/{}", url, v, key);
+    'outer: for i in 0..year_links_abs.len() {
+        for course_name in parse_year(&year_bodies[i]) {
+            let curr_url = format!("{}/{}/predmet/{}", url, year_links[i], course_name);
 
-            parsed_years.push(v.clone());
-            parsed_courses.push(key.clone());
+            parsed_years.push(year_links[i].clone());
+            parsed_courses.push(course_name.clone());
             parsed_urls.push(curr_url.clone());
 
             counter += 1;
@@ -67,7 +111,6 @@ fn main() {
                 break 'outer;
             }
         }
-        println!();
     }
 
     let requested_course_bodies: Vec<String> = helper(parsed_urls, &cookie1, &cookie2);
@@ -81,6 +124,4 @@ fn main() {
         file.write_all(&requested_course_bodies[i].as_bytes())
             .unwrap();
     }
-
-    println!("\nCounter is {}", counter);
 }
